7317b7879d34c21ea2fd7fd3b2bd4ec6914f9e22
nishikigoi <nishikigoi04@gmail.com>
2019-07-29 17:14:24 +0900

    implement back_home function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,37 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const request = require('request');
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.backHome = functions.https.onRequest(async (req, res) => {
+  const getTimeString = () => {
+    const localTimeOffset = 9 * 60 * 60 * 1000;
+    const dt = new Date(+new Date() + (localTimeOffset + 45 * 60 * 1000));
+    const hh = dt.getHours().toString();
+    const m = dt.getMinutes();
+    let mm;
+    if (m < 10) {
+      mm = '0' + m.toString();
+    } else {
+      mm = m.toString();
+    }
+    return hh + ':' + mm;
+  };
+  const options = {
+    uri: 'https://maker.ifttt.com/trigger/back_home/with/key/dncSJ3OPY_i10ZtwJNG68S',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    json: {
+      'value1': getTimeString()
+    }
+  };
+  request.post(options, function(error, response, body){});
+  res.status(200).send('Success');
+});
